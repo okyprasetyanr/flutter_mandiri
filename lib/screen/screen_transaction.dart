ff964b249eadaf306b534df03fe3458402fcea25
bluecode_breeze_android <email@example.com>
2025-08-25 12:10:59 +0700

    kantor-Transaction

--- lib/screen/screen_transaction.dart
+++ lib/screen/screen_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mandiri/style_and_transition/style/style_font_size.dart';
 import 'package:flutter_mandiri/template_responsif/layout_top_bottom_standart.dart';
 
 class ScreenTransaction extends StatefulWidget {
@@ -10,8 +11,27 @@ class ScreenTransaction extends StatefulWidget {
 
 class _ScreenTransactionState extends State<ScreenTransaction> {
   bool isOpen = false;
+  int gridviewcount = 0;
+  final List<String> dummyItems = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+    "Item 6",
+    "Item 7",
+    "Item 8",
+    "Item 9",
+    "Item 10",
+    "Item 11",
+    "Item 12",
+    "Item 13",
+  ];
+
   @override
   Widget build(BuildContext context) {
+    Orientation rotasi = MediaQuery.of(context).orientation;
+    gridviewcount = rotasi == Orientation.portrait ? 3 : 4;
     return LayoutTopBottom(
       heightRequested: 2,
       widthRequested: 2,
@@ -25,18 +45,97 @@ class _ScreenTransactionState extends State<ScreenTransaction> {
     return Column(
       children: [
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
           children: [
-            ElevatedButton.icon(
-              onPressed: () {
-                setState(() {
-                  isOpen = !isOpen;
-                });
-              },
-              icon: Icon(Icons.menu),
-              label: Text("Menu"),
+            const SizedBox(width: 10),
+            Flexible(
+              fit: FlexFit.loose,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    isOpen = !isOpen;
+                  });
+                },
+                icon: Icon(Icons.menu),
+                label: Text("Menu"),
+              ),
             ),
-            Text(""),
+            const SizedBox(width: 10),
+            Expanded(
+              flex: 2,
+              child: TextField(
+                decoration: InputDecoration(
+                  alignLabelWithHint: false,
+                  label: Text("Search", style: lv1TextStyle),
+                  hint: Text("Search...", style: lv1TextStyle),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Material(
+              elevation: 2,
+              borderRadius: BorderRadius.circular(5),
+              clipBehavior: Clip.antiAlias,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(5),
+                onTap: () {
+                  // aksi di sini
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Icon(Icons.qr_code_2_rounded, size: 35),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Material(
+              elevation: 2,
+              borderRadius: BorderRadius.circular(5),
+              clipBehavior: Clip.antiAlias,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(5),
+                onTap: () {},
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Icon(Icons.shopping_bag_rounded, size: 35),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
           ],
+        ),
+        SizedBox(height: 10),
+        Flexible(
+          fit: FlexFit.loose,
+          child: GridView.builder(
+            padding: EdgeInsets.all(10),
+            itemCount: dummyItems.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: gridviewcount,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+            ),
+            itemBuilder: (context, index) {
+              return Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(10),
+                child: InkWell(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      Flexible(child: Image.asset("assets/logo.png")),
+                      SizedBox(height: 5),
+                      Text(dummyItems[index]),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ],
     );
